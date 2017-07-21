ae22af3c09c1c7edd3195ce9d9de5083f6163d92
Naota <neverknowsbestfilms@gmail.com>
2017-07-20 21:00:12 -0400

    added javascript

--- app.js
+++ app.js
@@ -1,180 +1,227 @@
-var renderer = new THREE.WebGLRenderer({
-  antialias: true
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-renderer.shadowMapEnabled = true
+function app() {
+  // DROPDOWN MENU VARIABLES
+  var ddAries = $('#aries');
+  var ddTaurus = $('#taurus');
+  var ddGemini = $('#gemini');
+  var ddCancer = $('#cancer');
+  var ddLeo = $('#leo');
+  var ddVirgo = $('#virgo');
+  var ddLibra = $('#libra');
+  var ddScorpio = $('#scorpio');
+  var ddSagittarius = $('#sagittarius');
+  var ddCapricorn = $('#capricorn');
+  var ddAquarius = $('#aquarius');
+  var ddPisces = $('#pisces');
 
-var updateFcts = [];
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100);
-camera.position.z = 3;
+  var ddMenu = [ddAries, ddTaurus, ddGemini, ddCancer, ddLeo, ddVirgo, ddLibra,
+    ddScorpio, ddSagittarius, ddCapricorn, ddAquarius, ddPisces
+  ];
 
-var light = new THREE.AmbientLight(0x888888)
-scene.add(light)
-// var light	= new THREE.DirectionalLight( 'white', 1)
-// light.position.set(5,5,5)
-// light.target.position.set( 0, 0, 0 )
-// scene.add( light )
-
-var light = new THREE.DirectionalLight(0xcccccc, 1)
-light.position.set(5, 5, 5)
-scene.add(light)
-light.castShadow = true
-light.shadowCameraNear = 0.01
-light.shadowCameraFar = 15
-light.shadowCameraFov = 45
-
-light.shadowCameraLeft = -1
-light.shadowCameraRight = 1
-light.shadowCameraTop = 1
-light.shadowCameraBottom = -1
-// light.shadowCameraVisible	= true
-
-light.shadowBias = 0.001
-light.shadowDarkness = 0.2
-
-light.shadowMapWidth = 1024
-light.shadowMapHeight = 1024
-
-//////////////////////////////////////////////////////////////////////////////////
-//		add an object and make it move					//
-//////////////////////////////////////////////////////////////////////////////////
-
-function renderSun() {
-  var mesh = THREEx.Planets.createSun();
-  scene.add(mesh);
-}
-
-function renderMercury() {
-  var mesh = THREEx.Planets.createMercury();
-  scene.add(mesh);
-}
-
-function renderVenus() {
-  var mesh = THREEx.Planets.createVenus();
-  scene.add(mesh);
-}
-
-function renderEarth() {
-  var mesh = THREEx.Planets.createMoon();
-  scene.add(mesh);
-  var earthMesh = THREEx.Planets.createEarth();
-  scene.add(earthMesh);
-  var mesh = THREEx.Planets.createEarthCloud();
-  scene.add(mesh);
-  updateFcts.push(function(delta, now) {
-    mesh.rotation.y += 1 / 8 * delta;
-  });
-}
-
-function renderMars() {
-  var mesh = THREEx.Planets.createMars();
-  scene.add(mesh);
-}
-
-function renderJupiter() {
-  var mesh = THREEx.Planets.createJupiter();
-  scene.add(mesh);
-}
-
-function renderSaturn() {
-  var mesh = THREEx.Planets.createSaturn();
-  mesh.receiveShadow = true;
-  mesh.castShadow = true;
-  scene.add(mesh);
-  var mesh = THREEx.Planets.createSaturnRing();
-  mesh.receiveShadow = true;
-  mesh.castShadow = true;
-  scene.add(mesh);
-}
+  // //RENDER OPTION BAR
+  // function renderOptions() {
+  //
+  //   var template = _.template(
+  //     "<div class='container-fluid'>" +
+  //     "<div class='row options-row'>" +
+  //
+  //     "<div class='col-xs-2 box options op1'>" +
+  //     "<center>" + "Kudos" +
+  //     "</center>" +
+  //     "</div>" +
+  //
+  //     "<div class='col-xs-2 box options op2'>" +
+  //     "<center>" + "Elle" +
+  //     "</center>" +
+  //     "</div>" +
+  //
+  //     "<div class='col-xs-2 box options op3'>" +
+  //     "<center>" + "Ganesha" +
+  //     "</center>" +
+  //     "</div>" +
+  //
+  //     "<div class='col-xs-2 box options op4'>" +
+  //     "<center>" + "Retro" +
+  //     "</center>" +
+  //     "</div>" +
+  //
+  //     "</div>" +
+  //     "</div>"
+  //   )
+  //   $('#options').append(template());
+  // }
 
 
-function renderUranus() {
-  var mesh = THREEx.Planets.createUranus();
-  mesh.receiveShadow = true;
-  mesh.castShadow = true;
-  scene.add(mesh);
-  var mesh = THREEx.Planets.createUranusRing();
-  mesh.receiveShadow = true;
-  mesh.castShadow = true;
-  scene.add(mesh);
-}
+    //RENDER AZTRO RESULTS
+    function renderAztro(result) {
 
-function renderNeptune() {
-  var mesh = THREEx.Planets.createNeptune();
-  scene.add(mesh);
-}
+      var template = _.template(
+        "<div class='row aztro-row'>" +
+        "<div class='col-md-4 box aztro-content'>" +
+        "<p id='aztro-head'>" + "kudosmedia" + "</p>" +
+        "<p id='aztro-horo'>" + "<i>" + "<%= description %>" + "</i>" + "</p>" +
+        "</div>" +
+        "</div>" + // <div row>
+      )
+      //append to results div
+      $("#results").append(template(result));
+    }
+
+    //RENDER ELLE RESULTS
+    function renderElle(result) {
+
+      var template = _.template(
+        "<div class='row elle-row'>" +
+        "<div class='col-md-4 box elle-content'>" +
+        "<p id='elle-head'>" + "Elle" + "</p>" +
+        "<p id='elle-horo'>" + "<i>" + "." + "</i>" + "</p>" +
+        "</div>" +
+        "</div>"
+      )
+      //append to results div
+      $("#results").append(template(result))
+    }
+
+    //RENDER GANESHA RESULTS
+    function renderGanesha(result) {
+      var template = _.template(
+        "<div class='row ganesha-row'>" +
+        "<div class='col-md-4 box ganesha-content'>" +
+        "<p id='ganesha-head'>" + "GaneshaSpeaks" + "</p>" +
+        "<p id='ganesha-horo'>" + "<i>" + "<%= horoscope %>" + "</i>" + "</p>" +
+        "</div>" +
+        "</div>" + // <div row>
+      )
+      //append to results div
+      result.horoscope = result.horoscope.replace(/\['|'\]|\\r\\n/g, '');
+      $("#results").append(template(result))
+
+    }
+
+    //RENDER RETROGRADE RESULTS
+    function renderRetrograde(result) {
+        "<div class='row retro-row'>" +
+        "<div class='col-md-4 box retro-content'>" +
+        "<p id='retro-head'>" + "Is Mercury retrograde?" + "</p>" +
+        "<p id='retro-text'>" + "<i>" +
+        "<%= is_retrograde ? 'Yes!' : 'No!' %>" +
+        "</i>" + "</p>" +
+        "</div>" +
+        "</div>" + // <div row>
+      )
+      //append to results div
+      $("#results").append(template(result))
+    }
+
+    //DROPDOWN MENU EVENT LISTENER & API QUERIES
+    ddMenu.forEach(function(item) {
+      item.on("click", function(event) {
+        event.preventDefault();
+        //CLEAR RESULTS
+        // $("#options").empty(); //Clearing options
+        $("#results").empty(); //Clearing results
+
+        //OPTIONS BAR
+        // renderOptions();
 
 
-function renderPluto() {
-  var mesh = THREEx.Planets.createPluto();
-  scene.add(mesh);
-}
+        //AZTRO
+        console.log("Querying aztro...");
 
-var randomPlanet = {
-  1: renderSun(),
-  2: renderMercury(),
-  3: renderVenus(),
-  4: renderEarth(),
-  5: renderMars(),
-  6: renderJupiter(),
-  7: renderSaturn(),
-  8: renderUranus(),
-  9: renderNeptune(),
-  10: renderPluto()
-};
+        $.ajax({
+          type: 'POST',
+          url: 'https://aztro.herokuapp.com?sign=' + item[0].id + '&day=today',
+          success: function(data) {
+            console.log(data);
+            // An array of search results
+            renderAztro(data);
+          }
+        })
 
-var random = Math.floor((Math.random() * 10) + 1);
+        //ELLE
+        console.log("Querying Elle...");
 
-var mesh = THREEx.Planets.createStarfield();
-scene.add(mesh);
+        $.ajax({
+          type: 'GET',
+          url: 'elle.hearst.io',
+          success: function(data) {
+            console.log(data);
 
-randomPlanet[random];
+            // An array of search results
+            renderElle(data);
+          }
+        })
 
-updateFcts.push(function(delta, now) {
-  //mesh.rotation.x += 1 * delta;
-  //mesh.rotation.y += 1/2 * delta;
-})
+        //GANESHA
+        console.log("Querying Ganesha...");
 
-//////////////////////////////////////////////////////////////////////////////////
-//		Camera Controls							//
-//////////////////////////////////////////////////////////////////////////////////
-var mouse = {
-  x: 0,
-  y: 0
-}
-document.addEventListener('mousemove', function(event) {
-  mouse.x = (event.clientX / window.innerWidth) - 0.5
-  mouse.y = (event.clientY / window.innerHeight) - 0.5
-}, false)
-updateFcts.push(function(delta, now) {
-  camera.position.x += (mouse.x * 5 - camera.position.x) * (delta * 3)
-  camera.position.y += (mouse.y * 5 - camera.position.y) * (delta * 3)
-  camera.lookAt(scene.position)
-})
+          $.ajax({
+            type: 'GET',
+            // dataType: 'jsonp',
+            url: 'https://crossorigin.me/http://horoscope-api.herokuapp.com/horoscope/today/' + item[0].id,
+            success: function(data) {
+              console.log(data);
+              renderGanesha(data);
+
+            }
+          })
 
 
-//////////////////////////////////////////////////////////////////////////////////
-//		render the scene						//
-//////////////////////////////////////////////////////////////////////////////////
-updateFcts.push(function() {
-  renderer.render(scene, camera);
-})
+        //RETROGRADE
+        console.log("Querying Mercury Retrograde...")
 
-//////////////////////////////////////////////////////////////////////////////////
-//		loop runner							//
-//////////////////////////////////////////////////////////////////////////////////
-var lastTimeMsec = null
-requestAnimationFrame(function animate(nowMsec) {
-  // keep looping
-  requestAnimationFrame(animate);
-  // measure time
-  lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60
-  var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
-  lastTimeMsec = nowMsec
-  // call each update function
-  updateFcts.forEach(function(updateFn) {
-    updateFn(deltaMsec / 1000, nowMsec / 1000)
-  })
-})
+          $.ajax({
+            type: 'GET',
+            url: 'https://mercuryretrogradeapi.com',
+            success: function(data) {
+              renderRetrograde(data);
+
+            }
+          })
+
+
+
+        // //DELETE OPTION
+        // var op1 = $('.op1');
+        // var op2 = $('.op2');
+        // var op3 = $('.op3');
+        // var op4 = $('.op4');
+        //
+        //
+        // op1.on("click", function(event) {
+        //     $('.aztro-container').empty();
+        //     op1.remove();
+        //   });
+        //
+        //
+        // op2.on("click", function(event) {
+        //     $('.elle-container').empty();
+        //     op2.remove();
+        //   });
+        //
+        //
+        // op3.on("click", function(event) {
+        //     $('.ganesha-container').empty();
+        //     op3.remove();
+        //   });
+        //
+        //
+        // op4.on("click", function(event) {
+        //     $('.retro-container').empty();
+        //     op4.remove();
+        //   });
+        //
+        //
+
+
+
+      })
+    })
+  }
+  //
+  //
+  //
+
+
+
+
+  $(app)
