0bb8c1ca7f838adaea4b16eafbb46690060c879f
Naota <neverknowsbestfilms@gmail.com>
2018-03-23 13:50:53 -0400

    fix for deprecated code

--- app.js
+++ app.js
@@ -16,50 +16,7 @@ function app() {
   var ddMenu = [ddAries, ddTaurus, ddGemini, ddCancer, ddLeo, ddVirgo, ddLibra,
     ddScorpio, ddSagittarius, ddCapricorn, ddAquarius, ddPisces
   ];
-
-    //MARQUEE FUNCTION
-    function marquee(a, b) {
-        var width = b.width();
-        var start_pos = a.width();
-        var end_pos = -width;
-
-        function scroll() {
-            if (b.position().left <= -width) {
-                b.css('left', start_pos);
-                scroll();
-            }
-            else {
-                time = (parseInt(b.position().left, 10) - end_pos) *
-                    (10000 / (start_pos - end_pos)); // Increase or decrease speed by changing value 10000
-                b.animate({
-                    'left': -width
-                }, time, 'linear', function() {
-                    scroll();
-                });
-            }
-        }
-
-        b.css({
-            'width': width,
-            'left': start_pos
-        });
-        scroll(a, b);
-
-        b.mouseenter(function() {     // Remove these lines
-            b.stop();                 //
-            b.clearQueue();           // if you don't want
-        });                           //
-        b.mouseleave(function() {     // marquee to pause
-            scroll(a, b);             //
-        });                           // on mouse over
-
-    }
-
-    //RENDER MARQUEE FUNCTION
-    function renderMarquee() {
-        marquee($('#horoscopes'), $('#results'));  //Enter name of container element & marquee element
-    }
-
+  
     //RENDER AZTRO RESULTS
     function renderAztro(result) {
       var template = _.template(
@@ -67,7 +24,6 @@ function app() {
       )
       //append
       $("#results").append(template(result));
-      renderMarquee();
     }
 
     //RENDER GANESHA RESULTS
@@ -78,7 +34,6 @@ function app() {
       //append
       result.horoscope = result.horoscope.replace(/\['|'\]|\\r\\n/g, '');
       $("#results").append(template(result));
-      renderMarquee();
     }
 
     //RENDER RETROGRADE RESULTS
@@ -90,7 +45,6 @@ function app() {
       )
       //append
       $("#results").append(template(result))
-      renderMarquee();
     }
 
     //DROPDOWN MENU EVENT LISTENER & API QUERIES
